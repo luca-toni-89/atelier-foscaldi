bf2f4f032380166e7cc6bedf4a4b53a750ef6718
luca-toni-89 <luca_dj@live.de>
2026-08-28 07:37:24 +0200

    Include editable sections in reference schema

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 -- Referenzschema; die verbindliche, versionierte Definition befindet sich in migrations/0001_initial.sql.
 .read migrations/0001_initial.sql
+.read migrations/0002_editable_site_sections.sql
